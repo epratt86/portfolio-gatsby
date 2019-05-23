f1260fdc2066fcac8c85b5cc943ef804a66ee56e
Eric Pratt <ericpratt86@gmail.com>
2019-05-22 18:53:36 -0700

    spacing adj

--- src/pages/index.js
+++ src/pages/index.js
@@ -17,20 +17,19 @@ const IndexPage = () => {
         <div className={overlay}>
           <div className="container">
             <div className={row}>
-              <div className={col} />
               <div className={col}>
                 <Typing hideCursor="true">
                   <div>
                     <Typing.Speed ms={30} />
                     <h1> welcome to my portfolio. </h1>
                     <Typing.Delay ms={1000} />
-                    <Typing.Speed ms={30} />
+                    <Typing.Speed ms={20} />
                     <h3> home to past work and some of my thoughts. </h3>
                     <Typing.Delay ms={500} />
-                    <Typing.Speed ms={30} />
+                    <Typing.Speed ms={20} />
                     <h5>take a look into my life... </h5>
                     <Typing.Delay ms={500} />
-                    <Typing.Speed ms={1} />
+                    <Typing.Speed ms={10} />
                     <p>- eric</p>
                   </div>
                 </Typing>
