9c05dbc468924bb01ec45e1c8022cb08d44127cb
Eric Pratt <ericpratt86@gmail.com>
2019-05-28 15:34:04 -0700

    header container/gray adj

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -12,13 +12,9 @@ try {
     "scroll",
     _(() => {
       if (window.scrollY > 200) {
-        document
-          .querySelector("header")
-          .setAttribute("style", "background: #000; opacity: 0.7")
+        document.querySelector("header").setAttribute("style", "opacity: 0.7")
       } else {
-        document
-          .querySelector("header")
-          .setAttribute("style", "background: transparent")
+        document.querySelector("header").setAttribute("style", "opacity: 1")
       }
     }, 300)
   )
@@ -31,34 +27,38 @@ const Nav = () => {
   return (
     <header className={header}>
       <Head />
-      <div className={brand}>
-        <Link to="/">
-          <IconContext.Provider value={{ style: { verticalAlign: "middle" } }}>
-            <h2>
-              <FaCode />e<span>Pratt</span>
-            </h2>
-          </IconContext.Provider>
-        </Link>
+      <div className="container d-sm-block d-md-flex justify-content-between">
+        <div className={brand}>
+          <Link to="/">
+            <IconContext.Provider
+              value={{ style: { verticalAlign: "middle" } }}
+            >
+              <h2>
+                <FaCode />e<span>Pratt</span>
+              </h2>
+            </IconContext.Provider>
+          </Link>
+        </div>
+        <nav className={nav}>
+          <ul>
+            <Link to="/" activeClassName={activeNavItem}>
+              <li>Home</li>
+            </Link>
+            <Link to="/work" activeClassName={activeNavItem}>
+              <li>Work</li>
+            </Link>
+            <Link to="/about" activeClassName={activeNavItem}>
+              <li>About</li>
+            </Link>
+            <Link to="/blog" activeClassName={activeNavItem}>
+              <li>Blog</li>
+            </Link>
+            <Link to="/contact" activeClassName={activeNavItem}>
+              <li>Contact</li>
+            </Link>
+          </ul>
+        </nav>
       </div>
-      <nav className={nav}>
-        <ul>
-          <Link to="/" activeClassName={activeNavItem}>
-            <li>Home</li>
-          </Link>
-          <Link to="/work" activeClassName={activeNavItem}>
-            <li>Work</li>
-          </Link>
-          <Link to="/about" activeClassName={activeNavItem}>
-            <li>About</li>
-          </Link>
-          <Link to="/blog" activeClassName={activeNavItem}>
-            <li>Blog</li>
-          </Link>
-          <Link to="/contact" activeClassName={activeNavItem}>
-            <li>Contact</li>
-          </Link>
-        </ul>
-      </nav>
     </header>
   )
 }
