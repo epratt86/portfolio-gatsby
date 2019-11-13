c6538aa6944b655130b88935efea1bd002964c44
Eric Pratt <ericpratt86@gmail.com>
2019-11-13 12:13:13 -0800

    reword

--- src/pages/work.js
+++ src/pages/work.js
@@ -18,9 +18,7 @@ const WorkPage = () => {
             <div className="col">
               <h1 className="display-4 text-center">Work</h1>
               <div className="bottom-line" />
-              <h4 className="text-center mb-4">
-                Check out some of my previous projects
-              </h4>
+              <h4 className="text-center mb-4">Some of my previous projects</h4>
             </div>
           </div>
           <div className="row">
